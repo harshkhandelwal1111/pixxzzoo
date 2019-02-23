a0eca89743707bb358687da075f9122fad71ee30
harshkhandelwal1111 <harsh.khandelwal2017@vitstudent.ac.in>
2019-02-23 13:50:01 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    path = require('path');
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+// mongoose.connect('mongodb://readwriteuser_1:pixxzzoo@pixxzzoo-shard-00-00-ln0yh.mongodb.net:27017,pixxzzoo-shard-00-01-ln0yh.mongodb.net:27017,pixxzzoo-shard-00-02-ln0yh.mongodb.net:27017/test?ssl=true&replicaSet=Pixxzzoo-shard-0&authSource=admin&retryWrites=true', {useNewUrlParser: true});
+
+mongoose.connect("mongodb://localhost:27017/pixxzzoo", {useNewUrlParser: true});
+
+var contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+var Contact = mongoose.model("Contact", contactSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+"/public/index.html"));
+});
+
+app.post("/", (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var subject = req.body.subject;
+    var message = req.body.message;
+    var newContact = {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    }
+
+    Contact.create(newContact, (err, newlyCreated) => {
+        if(err) {
+            res.redirect("/");
+        } else {
+            res.redirect("/");
+            
+        }
+    })
+});
+
+app.listen(3000, () => {
+    console.log("The PIXXZZOO server has started!! at 3000");
+})
+
---
+++ public/assets/index.js
@@ -0,0 +1,68 @@
+// function ScrollToDiv(e) {
+//     console.log(e);
+//     (document.getElementsByClassName(e)[0]).scrollIntoView(true);
+// }
+
+// function ScrollToDiv(e)
+// {
+//  $('html,body').animate(
+//  {
+//   scrollTop: $("."+e).offset().top
+//  },
+//  'slow');
+// }
+
+$(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        
+        var sectionOffset = $(this.hash).offset().top - 20;
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      });
+      
+    });
+  });
+
+  $(document).ready(function() {
+      $("#myCarousel").on("slide.bs.carousel", function(e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 4;
+        var totalItems = $(".carousel-item").length;
+    
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+              $(".carousel-item")
+                .eq(i)
+                .appendTo(".carousel-inner");
+            } else {
+              $(".carousel-item")
+                .eq(0)
+                .appendTo($(this).find(".carousel-inner"));
+            }
+          }
+        }
+      });
+  });
+  
